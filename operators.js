8b530a71f940a414e10f2f11bd6ea4241919d733
AryanYadav <aryan.yofficial@gmail.com>
2026-08-09 19:57:35 +0530

    Operators in JS

---
+++ operators.js
@@ -0,0 +1,32 @@
+let a = 12
+let b = 6 
+console.log("Operators in JavaScript")
+console.log("a + b = ", a+b)
+console.log("a * b = ", a*b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+a++
+console.log(a)
+
+console.log(5 == 4) // false
+let c = 5
+let d = 7 
+console.log(c == d) // true 
+let e = 7
+let f = "7"
+
+console.log(e == f) // it will give true since both the values are same
+console.log(e === f) // it will give false because values are same but type is different 
+
+let A = 45
+let B = 34
+
+console.log(A > b) // true
+console.log(B > A) // false
+console.log( A >= B) // true 
+
+// let age = 17
+// let result = age >= 18? "adult" : "minor"
+// console.log(result)
