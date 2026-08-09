4bb5ea980c6467077b8a49bacf2804f157f79344
AryanYadav <aryan.yofficial@gmail.com>
2026-08-09 13:24:19 +0530

    Introduction to the JS(Java Script)

---
+++ introduction.js
@@ -0,0 +1,6 @@
+// Author: Aryan 
+// Age: 1
+
+console.log("Hello JS")
+console.log("Hey what's up JS")
+// console.log("Hey have you seen that move when you know") DOUBLE FORWARD SLASH FOR COMMENT
