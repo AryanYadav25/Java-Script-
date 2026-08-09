55fab3b989bfae495ae12e8ffacb3db84fa3b033
AryanYadav <aryan.yofficial@gmail.com>
2026-08-09 14:11:02 +0530

    Learned Variables in JS

---
+++ variables.js
@@ -0,0 +1,17 @@
+alert('Welcome to Variables')
+console.log("I am variable")
+
+let a = "Aryan"
+let b = 69
+const z = "Nitro"
+// z = "Hero" //  Assignment to constant variable (this is the error you will be seeing because you can't change const value unline let)
+console.log(b)
+
+b = 54 // Updated the value of b thats why it will print 54 in next console.log(b)
+console.log(b)
+
+b = "Super"
+
+console.log(a)
+console.log(b)
+console.log(z)
