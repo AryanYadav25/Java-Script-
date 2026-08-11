221ac7fd2d39dcc0cdb0061f7eeba8ff99a3eb29
AryanYadav <aryan.yofficial@gmail.com>
2026-08-11 12:37:20 +0530

    New methods of array in JS

--- arrays.js
+++ arrays.js
@@ -31,3 +31,52 @@ console.log(students)
 students[2].name = "Dimitri" // changing name 
 console.log(typeof students[1])
 console.log(students[1])
+
+// Methods in array
+
+let array = [12,13,14,15,16,17,18]
+array.push(20) // add new data to the array but at the end 
+console.log(array)
+array.pop() // remove the last element or data from the array
+console.log(array)
+array.reverse()
+console.log(array) // reverse the array data or elements
+array.shift()  // removes the first item of the array 
+console.log(array)
+array.unshift("Aryan")   // add items to the first place at 0 index 
+console.log(array)
+
+
+for(i = 0; i < array.length; i++){
+    const value = array[i]
+    console.log(value)
+}
+
+for (const key of array){
+    console.log(key)
+}
+
+let dupe_array = [4,5,6,7,8]
+const square = (a) =>{
+    return a*a
+}
+
+let new_dupe = dupe_array.map(square)
+console.log(new_dupe)
+
+let score = [12,45,23,54,234,68,67,56,98,88]
+const above_60 = (a) =>{
+    if(a> 60){
+        return true
+    }
+    else{
+       return false
+    }
+}
+
+let score_check = score.filter(above_60)
+console.log(score_check)
+
+
+let passed = score.filter(score => score >= 70)
+console.log(passed)
