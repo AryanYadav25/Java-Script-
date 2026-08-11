ca7e2335624d877405ad2b46ae2f845cf5f117e6
AryanYadav <aryan.yofficial@gmail.com>
2026-08-11 18:57:34 +0530

    Template Literals

---
+++ template.js
@@ -0,0 +1,7 @@
+console.log("heyyyyy whats'up guys")
+let name = "Aryan"
+let marks = 99
+// let last = `is a good boy and got x marks in CS `
+
+let final_string = `${name} is a good boy and got ${marks} in CS`
+console.log(final_string)
