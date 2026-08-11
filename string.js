5bc6f8fae75c42f4f7a7ff179cf78faf283d1367
AryanYadav <aryan.yofficial@gmail.com>
2026-08-11 18:42:06 +0530

    more on strigns

---
+++ string.js
@@ -0,0 +1,19 @@
+console.log("Heyyyyyyyyyy guyssssssssss")
+
+let name = "ARYAN"
+let music = `Twinkle Twinkle little star
+                -- by Aryan`
+console.log(music) 
+console.log(music.length)
+console.log(name[1]) // we can get elements like in arrays
+
+let me = "First"
+me[1] = "h" // we cannot change elements in strings 
+console.log(me)
+
+let a = "45"
+let b = 45 
+let c = "hey i am "
+let d = "Aryan"
+console.log(a+b) // it will print 4545 not 90 
+console.log(c+d) // it will print hey i am Aryan 
