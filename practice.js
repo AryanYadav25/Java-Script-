c03393dabadafabcbb8a9d66607a4c5561258eb1
AryanYadav <aryan.yofficial@gmail.com>
2026-08-11 15:44:59 +0530

    more question, kinda hard

--- practice.js
+++ practice.js
@@ -18,15 +18,24 @@ for(j = 0; j < array.length; j++){
 
 // Q3. print all keys using loop
 
-let info = [{name:"Aryan", class:"7th", age: 99, language:"JavaScript"}]
-for(key in info){
-    console.log("The key of aryan", key,"is",info[key])
+const info = {
+    name:"aryan",
+    age:"65",
+    language:"JS",
+}
+for(let key in info){
+    console.log(key)
 }
 
-obj = {name:"aryan", age:99, class:7,language:"JavaScript"}
-for(key in obj){
-    console.log("The value of aryan",key,"is",obj[key])
-}
+// let info = [{name:"Aryan", class:"7th", age: 99, language:"JavaScript"}]
+// for(key in info){
+//     console.log("The key of aryan", key,"is",info[key])
+// }
+
+// obj = {name:"aryan", age:99, class:7,language:"JavaScript"}
+// for(key in obj){
+//     console.log("The value of aryan",key,"is",obj[key])
+// }
 
 // O4. count object properties
 const person ={
@@ -35,6 +44,12 @@ const person ={
     age:60,
     langaueg:"JS"
 }
+
+let count = 0
+for(let key in person){
+    count++
+}
+console.log(count)
 
 
 // O5. double every number
@@ -49,4 +64,44 @@ console.log(new_array)
 
 // O6. Add 10 to every number 
 
-let me = []
+let me = [0,1,2,3,4,5,6,7,8,9,10]
+const add_10 = (a) =>{
+    return a+10
+}
+
+let me_10 = me.map(add_10)
+console.log(me_10)
+
+// O7. get only even numbers 
+
+// let numb = [1,2,3,4,5,6,7,8,9,10]
+// const numb_even = (a) =>{
+//     if(numb[a]%2 == 0){
+//         console.log(numb[a])
+//     }
+// }
+
+// let num_even = numb.filter(numb_even)
+
+// O8. get number greater than 50 
+
+let gr8 = [20, 75, 43, 90, 12, 65, 31]
+const gt50 = gr8.filter(a => a > 50)
+console.log(gt50)
+
+// O9. get names longer than 5 characters
+
+const name =["aryan","mayank","adarsh","manvi","vanshika"]
+const great_5 = name.filter(a => a.length > 5)
+console.log(great_5)
+
+// O10. find adults in loop of obj
+
+let adults = [{name:"Aryan", age:24},{name:"Vanshika", age:23},{name:"dev",age:15}]
+const is_adult = adults.filter(a => a.age > 18)
+const is_name = adults.map(a => a.name)
+console.log(is_adult)
+console.log(is_name)
+
+// O11. get names of adult 
+
